07534f7743bec8775e6eb671f9eefe770d439d81
msakuta <masahiro.sakuta@gmail.com>
2024-02-04 18:28:34 +0900

    Render background tiles with canvas

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,7 @@
 mod utils;
 
 use wasm_bindgen::prelude::*;
-use web_sys::CanvasRenderingContext2d;
+use web_sys::{CanvasRenderingContext2d, HtmlImageElement};
 
 #[macro_export]
 macro_rules! console_log {
@@ -30,12 +30,19 @@ pub fn say_hello() {
 }
 
 #[wasm_bindgen]
-pub fn render(
-    context: &CanvasRenderingContext2d,
-) -> Result<(), JsValue> {
+pub fn render(context: &CanvasRenderingContext2d, img: &HtmlImageElement) -> Result<(), JsValue> {
     // let width = context.cli();
     // context.clear_rect(0., 0., 32., 32.);
     context.set_fill_style(&JsValue::from("#ff0000"));
+    for iy in 0..5 {
+        let y = iy as f64 * 32.;
+        for ix in 0..10 {
+            let x = ix as f64 * 32.;
+            context.draw_image_with_html_image_element_and_sw_and_sh_and_dx_and_dy_and_dw_and_dh(
+                img, 0., 0., 32., 32., x, y, 32., 32.,
+            )?;
+        }
+    }
     // context.fill_rect(0., 0., 32., 32.);
     // if let Some(item) = self.tool_belt.get(tool_index).unwrap_or(&None) {
     //     if Some(SelectedItem::ToolBelt(tool_index)) == self.selected_item {
--- src/main.js
+++ src/main.js
@@ -2,14 +2,11 @@ import bg from '../images/back32.png';
 
 (async () => {
     const wasm = await import("../Cargo.toml")
-    const {say_hello, render} = await wasm.default()
-    const canvas = document.getElementById('canvas')
-    const ctx = canvas.getContext('2d')
-    const res = await fetch(bg);
+    const {render} = await wasm.default()
+    const canvas = document.getElementById('canvas');
+    const ctx = canvas.getContext('2d');
     const img = await loadImage(bg);
-    ctx.drawImage(img, 0, 0);
-    render(ctx)
-    say_hello("dozo")
+    render(ctx, img);
 })()
 
 async function loadImage(url) {
