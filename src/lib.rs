3893622305223b44a9a474302fd6d716b9d9c39a
msakuta <masahiro.sakuta@gmail.com>
2024-02-04 18:46:54 +0900

    Add state variable to the game

--- src/lib.rs
+++ src/lib.rs
@@ -25,35 +25,64 @@ extern "C" {
 }
 
 #[wasm_bindgen]
-pub fn say_hello() {
-    console_log!("Hello, asteroid-colonies!");
+pub struct AsteroidColonies {
+    buildings: Vec<[i32; 2]>,
 }
 
 #[wasm_bindgen]
-pub fn render(context: &CanvasRenderingContext2d, img: &HtmlImageElement) -> Result<(), JsValue> {
-    // let width = context.cli();
-    // context.clear_rect(0., 0., 32., 32.);
-    context.set_fill_style(&JsValue::from("#ff0000"));
-    for iy in 0..5 {
-        let y = iy as f64 * 32.;
-        for ix in 0..10 {
-            let x = ix as f64 * 32.;
-            context.draw_image_with_html_image_element_and_sw_and_sh_and_dx_and_dy_and_dw_and_dh(
-                img, 0., 0., 32., 32., x, y, 32., 32.,
-            )?;
+impl AsteroidColonies {
+    #[wasm_bindgen(constructor)]
+    pub fn new() -> Self {
+        Self {
+            buildings: vec![[3, 4]],
         }
     }
-    // context.fill_rect(0., 0., 32., 32.);
-    // if let Some(item) = self.tool_belt.get(tool_index).unwrap_or(&None) {
-    //     if Some(SelectedItem::ToolBelt(tool_index)) == self.selected_item {
-    //         context.set_fill_style(&js_str!("#00ffff"));
-    //         context.fill_rect(0., 0., 32., 32.);
-    //     }
-    //     let mut tool = self.new_structure(item, &Position { x: 0, y: 0 })?;
-    //     tool.set_rotation(&self.tool_rotation).ok();
-    //     for depth in 0..3 {
-    //         tool.draw(self, context, depth, true)?;
-    //     }
-    // }
-    Ok(())
+
+    pub fn render(
+        &self,
+        context: &CanvasRenderingContext2d,
+        img: &HtmlImageElement,
+        img2: &HtmlImageElement,
+    ) -> Result<(), JsValue> {
+        // let width = context.cli();
+        // context.clear_rect(0., 0., 32., 32.);
+        context.set_fill_style(&JsValue::from("#ff0000"));
+        for iy in 0..15 {
+            let y = iy as f64 * 32.;
+            for ix in 0..20 {
+                let x = ix as f64 * 32.;
+                context
+                    .draw_image_with_html_image_element_and_sw_and_sh_and_dx_and_dy_and_dw_and_dh(
+                        img, 0., 0., 32., 32., x, y, 32., 32.,
+                    )?;
+            }
+        }
+
+        for building in &self.buildings {
+            context.draw_image_with_html_image_element_and_sw_and_sh_and_dx_and_dy_and_dw_and_dh(
+                img2,
+                0.,
+                0.,
+                32.,
+                32.,
+                building[0] as f64 * 32.,
+                building[1] as f64 * 32.,
+                32.,
+                32.,
+            )?;
+        }
+        // context.fill_rect(0., 0., 32., 32.);
+        // if let Some(item) = self.tool_belt.get(tool_index).unwrap_or(&None) {
+        //     if Some(SelectedItem::ToolBelt(tool_index)) == self.selected_item {
+        //         context.set_fill_style(&js_str!("#00ffff"));
+        //         context.fill_rect(0., 0., 32., 32.);
+        //     }
+        //     let mut tool = self.new_structure(item, &Position { x: 0, y: 0 })?;
+        //     tool.set_rotation(&self.tool_rotation).ok();
+        //     for depth in 0..3 {
+        //         tool.draw(self, context, depth, true)?;
+        //     }
+        // }
+        Ok(())
+    }
 }
--- src/main.js
+++ src/main.js
@@ -1,12 +1,16 @@
 import bg from '../images/back32.png';
+import power from '../images/power.png';
 
 (async () => {
     const wasm = await import("../Cargo.toml")
-    const {render} = await wasm.default()
+    const {render, AsteroidColonies} = await wasm.default();
+
+    const game = new AsteroidColonies();
     const canvas = document.getElementById('canvas');
     const ctx = canvas.getContext('2d');
     const img = await loadImage(bg);
-    render(ctx, img);
+    const img2 = await loadImage(power);
+    game.render(ctx, img, img2);
 })()
 
 async function loadImage(url) {
