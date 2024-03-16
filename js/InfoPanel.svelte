3a4939182d95973e18758ad5ce02d8fd4d85188b
msakuta <masahiro.sakuta@gmail.com>
2024-03-16 22:06:14 +0900

    Remove unused styles

--- js/InfoPanel.svelte
+++ js/InfoPanel.svelte
@@ -36,25 +36,4 @@
       border: 1px solid black;
       background-color: #afafff;
     }
-
-    .recipes {
-      border: 1px solid black;
-      background-color: #afafaf;
-      padding: 4px;
-      z-index: 100;
-      position: fixed;
-      top: 50%;
-      left: 50%;
-      margin-right: -50%;
-      transform: translate(-50%, -50%);
-    }
-
-    .recipe {
-      position: relative;
-      margin: 4px;
-      padding: 4px;
-      border: 1px solid #7f7f3f;
-      background-color: #ffff7f;
-      white-space: normal;
-    }
 </style>
