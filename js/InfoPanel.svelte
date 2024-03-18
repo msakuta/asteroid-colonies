6df54eede8fac2ec0eadea778568409b7236ed5e
msakuta <masahiro.sakuta@gmail.com>
2024-03-19 00:12:41 +0900

    Adjust the UI of info panel a bit

--- js/InfoPanel.svelte
+++ js/InfoPanel.svelte
@@ -13,26 +13,26 @@
     let construction = null;
     let extra = "";
     $: {
-      let building = result?.building;
-      if (building) {
-        buildingType = building.type_;
-        task = building.task;
-        recipe = building.recipe;
-        inventory = building.inventory;
-        crews = formatCrews(building);
-      }
-      else {
-        buildingType = "";
-        task = "";
-        recipe = null;
-        inventory = new Map();
-        crews = "-";
-      }
-      construction = result?.construction;
+        let building = result?.building;
+        if (building) {
+            buildingType = building.type_;
+            task = building.task;
+            recipe = building.recipe;
+            inventory = building.inventory;
+            crews = formatCrews(building);
+        }
+        else {
+            buildingType = "";
+            task = "";
+            recipe = null;
+            inventory = new Map();
+            crews = "-";
+        }
+        construction = result?.construction;
 
-      // Time scale = 360
-      // 1 energy unit = 360 kJ = 0.36MJ
-      extra = result ? `Accumulated energy: ${(result.energy * 0.36).toFixed(2)} MJ
+        // Time scale = 360
+        // 1 energy unit = 360 kJ = 0.36MJ
+        extra = result ? `Accumulated energy: ${(result.energy * 0.36).toFixed(2)} MJ
 Power capacity: ${result.power_capacity} kW
 Power demand: ${result.power_demand} kW
 Power load: ${(result.power_demand / result.power_capacity * 100).toFixed(1)} %
@@ -41,7 +41,8 @@ Transports: ${result.transports}` : "";
 </script>
 
 <div class="bottomPanel">
-  <pre class="infoPanel">
+    <div class="header">Info Panel</div>
+    <pre class="infoPanel">
 Building: {buildingType}
 Task: {task}
 Recipe: {#if recipe}
@@ -63,12 +64,16 @@ Construction: {#if construction}
       position: absolute;
       left: 10px;
       bottom: 10px;
-      border: 1px solid black;
+      border: 3px outset #7D7D7D;
+      padding: 5px;
       background-color: #afafaf;
     }
 
+    .header {
+        font-weight: bold;
+    }
+
     .infoPanel {
-      border: 1px solid black;
-      background-color: #afafff;
+        margin: 0;
     }
 </style>
